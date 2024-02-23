ac250b9dd60227c6c79273424a8ee6f544d145b7
jackstockley89 <jstock8904@gmail.com>
2024-02-23 14:05:06 +0000

    feat(Go): add testing

---
+++ test/unit-test/success/keda.tf
@@ -0,0 +1,18 @@
+module "keda" {
+  source = "../../.."
+
+  enable_keda = true
+  keda_operator_replica_count = 3
+  keda_metrics_server_replica_count = 3
+  keda_webhook_replica_count = 3
+  keda_minimum_Available = 1
+  keda_maximum_Unavailable = 0
+
+  labels = {
+      business-unit = "Platforms"
+      application   = "cloud-platform-terraform-keda"
+      is-production = "true"
+      owner         = "Cloud Platform: platforms@digital.justice.gov.uk"
+      source-code   = "github.com/ministryofjustice/cloud-platform-terraform-keda"
+    }
+}
---
+++ test/unit-test/success/variables.tf
@@ -0,0 +1,9 @@
+variable "github_owner" {
+  description = "Required by the GitHub terraform provider"
+  default     = "ministryofjustice"
+}
+
+variable "github_token" {
+  description = "Required by the GitHub terraform provider"
+  default     = ""
+}
